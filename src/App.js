a46a8e877de9b6c69be113412e2ed18d72071092
Dean Norman <dean@22seven.com>
2017-10-08 14:10:05 +0200

    started adding counter logic

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   display:flex;
+  flex-direction: column;
   justify-content: center;
-  flex-wrap:  wrap;
+  align-items:center;
   background: #00ECB9;
   font-family: sans-serif;
   font-weight:100;
+  min-height:400px;
+  height:100%;
 `;
 
 const Title = styled.h1`
@@ -18,9 +21,7 @@ const Title = styled.h1`
 `;
 
 const ClockDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
+  display:flex;
   font-family: inherit;
   color:white;
   text-align: center;
@@ -29,10 +30,9 @@ const ClockDiv = styled.div`
 
 const TimeContainer = styled.div`
   display:flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
+  flex-direction: column;
   margin: 2px; 
-  padding: 10px;
+  padding: 10px 10px 0 10px;
   border-radius: 3px;
   background-color:#00BF96;
 `;
@@ -44,33 +44,74 @@ const TimeValue = styled.div`
 `;
 
 const TimeText = styled.div`
- flex-grow: 1;
-  padding-top: 5px;
+  padding: 15px;
   font-size: 16px;
 `;
 
 class App extends Component {
+state = {
+  endtime: '2017-12-8',
+}
+
+  getTimeRemaining = (endtime) => {
+    const time = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((time / 1000 / 60) % 60 );
+    const seconds = Math.floor((time / 1000) % 60);
+    
+    return {
+      time: time,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    }
+  }
+
+
+
   render() {
+    function updateClock(endtime) {
+      var t = this.getTimeRemaining(this.state.endtime);
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+    var timeinterval = setInterval(updateClock, 1000);
+    console.log(timeinterval)
     return (
       <Wrapper>
+        <Title> Parse Time </Title>
         <ClockDiv id="clockdiv">
-          <Title>Count down</Title>
           <TimeContainer>
-            <TimeValue class="days"></TimeValue>
+            <TimeValue class="days">
+              {
+                (this.getTimeRemaining('2017-12-8').days)
+              }
+            </TimeValue>
             <TimeText class="smalltext">Days</TimeText>
           </TimeContainer>
           <TimeContainer>
-            <TimeValue class="hours"></TimeValue>
+            <TimeValue class="hours">
+              {this.getTimeRemaining('2017-12-8').hours}
+            </TimeValue>
             <TimeText class="smalltext">Hours</TimeText>
           </TimeContainer>
           <TimeContainer>
-            <TimeValue class="minutes"></TimeValue>
+            <TimeValue class="minutes">
+              {this.getTimeRemaining('2017-12-8').minutes}
+            </TimeValue>
             <TimeText class="smalltext">Minutes</TimeText>
           </TimeContainer>
           <TimeContainer>
-            <TimeValue class="seconds"></TimeValue>
+            <TimeValue class="seconds">
+              {this.getTimeRemaining('2017-12-8').seconds}
+            </TimeValue>
             <TimeText class="smalltext">Seconds</TimeText>
           </TimeContainer>
+          
         </ClockDiv>
       </Wrapper>
     );
